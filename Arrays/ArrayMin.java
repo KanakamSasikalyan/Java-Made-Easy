5362ffcda75898f3c41e59ce40d8f16e685be264
SasiKalyan <kanakamsasikalyan1@gmail.com>
2022-02-04 06:35:54 +0530

    Create ArrayMin.java

---
+++ Arrays/ArrayMin.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class ArrayMin
+{
+    public static void main(String[] args) {
+        int[] arr = {1,5,3,7,9};
+        Arrays.sort(arr);
+        System.out.println(arr[0]);
+    }
+}
