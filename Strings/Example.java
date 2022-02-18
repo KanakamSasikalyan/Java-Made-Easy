dee9069d200898a105233f67d8bfa6ad37073226
SasiKalyan <kanakamsasikalyan1@gmail.com>
2022-02-18 22:34:36 +0530

    Add files via upload

---
+++ Strings/Example.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Example
+{
+    public static void main(String[] args) {
+        //Strings can be created by 2 ways
+	String str1 = "Java";
+	String str2 = new String("Java");
+    }
+}
